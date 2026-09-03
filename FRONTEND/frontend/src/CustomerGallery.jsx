cb0f1f3fe11c12d6526ad5e42fcf3c08466cc323
Prateek7421 <prateek4418@gmail.com>
2026-09-04 01:39:32 +0530

    Update CustomerGallery.jsx

--- FRONTEND/frontend/src/CustomerGallery.jsx
+++ FRONTEND/frontend/src/CustomerGallery.jsx
@@ -5,7 +5,7 @@ import {
 } from 'lucide-react';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://dcrocbackend.onrender.com/api';
 
 export default function CustomerGallery() {
   const [mediaItems, setMediaItems] = useState([]);
